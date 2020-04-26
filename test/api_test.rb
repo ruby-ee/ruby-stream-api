937ab2e8c568ceeb0ad7cabc5ef941c37d639ead
amihaiemil <amihaiemil@gmail.com>
2020-04-26 15:40:31 +0300

    rubocop refactoring

--- test/api_test.rb
+++ test/api_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
-class StreamApi::ApiTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::StreamApi::VERSION
+module ApiTest
+  class StreamApiTest < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil StreamApi::VERSION
+    end
   end
 end
