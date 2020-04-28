9b5f0ebf72a464836646b80db00a3b32addd259f
amihaiemil <amihaiemil@gmail.com>
2020-04-28 19:34:47 +0300

    #10 simplecov integrated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "stream.rb"
-
+require "fromarray.rb"
 require "minitest/autorun"
