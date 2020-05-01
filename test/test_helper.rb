0cc8ee2bc70b52e948e4cfdbb408c293929783fc
amihaiemil <amihaiemil@gmail.com>
2020-05-01 16:43:56 +0300

    coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
 require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
 SimpleCov.start do
   add_filter 'test'
   add_filter 'Rakefile'
 end
-require 'coveralls'
 Coveralls.wear!
-
 require "minitest/autorun"
