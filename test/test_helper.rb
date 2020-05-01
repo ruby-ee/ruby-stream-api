1452525724b221a869de4df600f457ca4e5160cf
amihaiemil <amihaiemil@gmail.com>
2020-05-01 16:59:58 +0300

    use CodeCov to display coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,10 @@
 require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-])
 SimpleCov.start do
   add_filter 'test'
   add_filter 'Rakefile'
 end
-Coveralls.wear!
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 require "minitest/autorun"
